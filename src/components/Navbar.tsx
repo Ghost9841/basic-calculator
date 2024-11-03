c2071dea63f81adb0cb90b5d0e2f374554a8329d
Ghost9841 <asubedi778@gmail.com>
2024-11-03 09:12:50 +0545

    First Commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="w-full justify-center items-center flex text-black bg-gray-500 p-8 text-xl ">
+        <div className="mx-4 ">Home</div>
+        <div className="mx-4">About</div>
+        <div className="mx-4">Contact</div>
+     
+    </div>
+  )
+}
+
+export default Navbar
